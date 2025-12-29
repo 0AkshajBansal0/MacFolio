c545b75f48263506f14f5ea3bae72faaedcf550c
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-12-30 01:17:20 +0530

    Feat: welcome text basic

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,19 @@
+import { useRef } from "react";
+
+const Welcome = () => {
+    const titleRef = useRef(null);
+    const subtitleRef = useRef(null);
+
+    return <section id="welcome">
+        <p ref={subtitleRef}>Hey, I'm Akshaj! Welcome to my</p>
+        <h1 ref={titleRef} className="mt-7">
+            Portfolio
+        </h1>
+
+        <div className="small-screen">
+            <p>This portfolio is designed for desktop/tablet screens only.</p>
+        </div>
+    </section>
+}
+
+export default Welcome
